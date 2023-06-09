1ed6cc1027375c65943514ab116c79aa693f24f1
Matthew DiTrolio <mditrolio@vestmark.com>
2023-06-09 10:45:29 -0400

    Updated test script to accept solution file parameter

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,3 @@
-dotnet test --logger:trx --results-directory:/src/TestResults /p:CollectCoverage=true /p:CoverletOutputFormat=json%2Clcov%2Copencover /p:CoverletOutput=/src/TestResults/lcov
+SOLUTION=$1
+
+dotnet test $SOLUTION --logger:trx --results-directory:/src/TestResults /p:CollectCoverage=true /p:CoverletOutputFormat=json%2Clcov%2Copencover /p:CoverletOutput=/src/TestResults/lcov
