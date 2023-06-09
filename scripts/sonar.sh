ce944d1987fd014f9351ae862f1eeb48f386f957
Matthew DiTrolio <mditrolio@vestmark.com>
2023-06-09 14:01:14 -0400

    Updated sonar script to accept solution file as parameter

--- scripts/sonar.sh
+++ scripts/sonar.sh
@@ -1,5 +1,6 @@
-SERVICE_NAME=$1
-SONAR_TARGET_BRANCH=$2
+SOLUTION=$1
+SERVICE_NAME=$2
+SONAR_TARGET_BRANCH=$3
 
 export PATH=$PATH:/home/jenkins/.dotnet/tools
 
@@ -12,5 +13,5 @@ dotnet sonarscanner begin \
     /d:sonar.cs.opencover.reportsPaths=/src/TestResults/lcov.opencover.xml \
     /d:sonar.branch.name=${BRANCH_NAME} \
     $SONAR_TARGET_BRANCH
-dotnet publish -c Release
+dotnet publish $SOLUTION -c Release
 dotnet sonarscanner end /d:sonar.login=${SONAR_AUTH_TOKEN}
